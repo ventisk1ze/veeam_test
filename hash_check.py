6675ac3bff379bd982ece20a566d1708678c13a1
ventisk1ze <gkpinchuk@hotmail.com>
2021-07-04 21:08:55 +1000

    Redone hash as a function with parameters of file and hash function

--- hash_check.py
+++ hash_check.py
@@ -1,16 +1,19 @@
-from hashlib import sha1, sha512, md5
-file = "source.xml"
-
-sha1_hash = sha1()
-sha512_hash = sha512()
-md5_hash = md5()
+from hashlib import sha1, sha256, md5
 
 BLOCK_SIZE = 65536
 
-with open(file, 'rb') as f:
-    fb = f.read(BLOCK_SIZE)
-    while len(fb) > 0:
-        sha1_hash.update(fb)
-        fb = f.read(BLOCK_SIZE)
 
-print(sha1_hash.hexdigest())
+def hash(file, hash_type):
+    if hash_type == "SHA1":
+        hash_object = sha1()
+    elif hash_type == "SHA256":
+        hash_object = sha256()
+    elif hash_type == "MD5":
+        hash_object = md5()
+    with open(file, 'rb') as f:
+        fb = f.read(BLOCK_SIZE)
+        while len(fb) > 0:
+            hash_object.update(fb)
+            fb = f.read(BLOCK_SIZE)
+    return hash_object.hexdigest()
+
