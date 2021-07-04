60f1474efdbf8fd9fd0081190f2ee0f60037c90c
ventisk1ze <gkpinchuk@hotmail.com>
2021-07-04 22:16:12 +1000

    Problem solved with hardcoded folder

--- hash_check.py
+++ hash_check.py
@@ -3,12 +3,13 @@ from hashlib import sha1, sha256, md5
 BLOCK_SIZE = 65536
 FOLDER = 'hash_data/'
 
+
 def hash(file, hash_type):
-    if hash_type == "SHA1":
+    if hash_type == "sha1":
         hash_object = sha1()
-    elif hash_type == "SHA256":
+    elif hash_type == "sha256":
         hash_object = sha256()
-    elif hash_type == "MD5":
+    elif hash_type == "md5":
         hash_object = md5()
     with open(file, 'rb') as f:
         fb = f.read(BLOCK_SIZE)
@@ -16,3 +17,14 @@ def hash(file, hash_type):
             hash_object.update(fb)
             fb = f.read(BLOCK_SIZE)
     return hash_object.hexdigest()
+
+
+input_file = open("input.txt", 'r', encoding='utf-8-sig')
+for line in input_file:
+    properties = line.split(" ")
+    properties[2] = properties[2].strip("\n")
+    path = FOLDER + properties[0]
+    if(hash(path, properties[1]) == properties[2]):
+        print(properties[0] + " " + "OK")
+    else:
+        print(properties[0] + " " + "FAIL")
